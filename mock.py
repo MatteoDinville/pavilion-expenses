fce0dde6d5e47e0fb4354397c7af2002939d6a2a
MatteoDinville <matteo.dinville@ynov.com>
2026-09-23 00:04:55 +0200

    test(mock): :rocket: mock data to simulate gemini IA extraction

---
+++ mock.py
@@ -0,0 +1,31 @@
+def mock_gemini_extraction():
+    return [
+        {
+            "numero_page": "1",
+            "date": "15/09/2026",
+            "facture_nom": "Papeterie Bureau Plus",
+            "montant": "24,90 €",
+            "categorie": "Fournitures de bureaux",
+        },
+        {
+            "numero_page": "2",
+            "date": "16/09/2026",
+            "facture_nom": "Maison Décor",
+            "montant": "45,50 €",
+            "categorie": "Fournitures décoration pavillon",
+        },
+        {
+            "numero_page": "3",
+            "date": "17/09/2026",
+            "facture_nom": "Carrefour",
+            "montant": "82,30 €",
+            "categorie": "Alimentation appartement",
+        },
+        {
+            "numero_page": "4",
+            "date": "18/09/2026",
+            "facture_nom": "Kiabi",
+            "montant": "59,99 €",
+            "categorie": "Habillement des Enfants",
+        },
+    ]
